4d8c229f6b7de67f90c8cd5d74f9ae75a7b985ba
bjead <blakejdean@gmail.com>
2018-11-04 22:42:52 +1100

    Add project files.

---
+++ GameOfLife/Cell.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    // Public enum (Global in application)
+    public enum Cell
+    {
+        Alive,
+        Dead
+    }
+}
